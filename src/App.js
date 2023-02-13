61b0a1bf666d9c71d70f3b7a879198277b628946
M Adji Pratama <104249587+ajips-code@users.noreply.github.com>
2023-02-13 12:11:44 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>Syuhada App</code> and give your changes.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go a head
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
